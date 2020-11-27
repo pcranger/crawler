9908e940a1207343700f40997ea579912884ce0e
pcranger <52710878+pcranger@users.noreply.github.com>
2020-11-27 14:35:18 +0700

    might work idk just commit

--- web.py
+++ web.py
@@ -18,34 +18,44 @@ def download(url):
     datetime = datetime.replace("/", "-")
     datetime = datetime.replace(":", "")
     # make a folder with datetime
-    if os.path.isdir(f'./{datetime}') == False:
-        os.mkdir(f'./{datetime}')
+    if os.path.isdir(f'./download/{datetime}') == False:
+        os.mkdir(f'./download/{datetime}')
     images = []
     n = len(vcs) - 1
     vcs = vcs[:n]
     for i, vc in enumerate(vcs):
-        # find caption and save to .txt
-        cap = vc.find('p').text
-        with open(f'./{datetime}/caption{i}.html', 'a', encoding='utf-8') as f:
-            f.write(cap)
         # find and save image
         try:
             image_url = vc.div.img['src']
         except:
             break
         img = Image.open(requests.get(image_url, stream=True).raw)
-        img.save(f'./{datetime}/image{i}.jpg')
+        if img.mode in ("RGBA", "P"):
+            img = img.convert("RGB")
+        img.save(f'./download/{datetime}/image{i}.jpg')
+
+        # find caption and save to .txt
+        try:
+            cap = vc.find('p').text
+            with open(f'./download/{datetime}/caption{i}.html', 'a', encoding='utf-8') as f:
+                f.write(cap)
+        except:
+            continue
+            # fix caption
 
 
 def exec():
-    urls = lmao.returndata()
-    print("________________________________________________________")
-    print("Downloading")
-    print("________________________________________________________")
-    for i in urls:
-        print("downloading " + i)
-        download(i)
-    pass
+    # urls = lmao.returndata()
+    # print("________________________________________________________")
+    # print("Downloading")
+    # print("________________________________________________________")
+    # if os.path.isdir('downloads') == False:
+    #     os.mkdir('download')
+    # for i in urls:
+    #     print("downloading " + i)
+    #     download(i)
+    # pass
+    download('https://tuoitre.vn/dia-thuc-an-bo-duong-cua-chuyen-gia-dinh-duong-harvard-20181210143108105.htm')
 
 
 exec()
