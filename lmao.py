c7728619f03435134aed9ed25f73788ed5233146
pcranger <52710878+pcranger@users.noreply.github.com>
2020-11-27 13:57:00 +0700

    upload my files

--- lmao.py
+++ lmao.py
@@ -2,35 +2,40 @@ import requests
 from bs4 import BeautifulSoup
 import os
 url1 = "https://tuoitre.vn/suc-khoe/dinh-duong/trang-1.htm"
+bottomurls = []
+pageurls = []
+urls = []
 
 
-def fetchpage(url, i):
+def fetchpage(url, i, bottomurls, pageurls):
+    print("Link from page #" + str(i))
     req = requests.get(url)
-    print(url)
     soup = BeautifulSoup(req.content, 'html.parser')
-    # focus-top
     baseurl = "https://tuoitre.vn"
 
+    # focus-top
     topurl = soup.find('a', class_='focus-top-title').attrs["href"]
 
     # focus-bottom
-    bottomurls = []
     bottomtags = soup.find_all('a', class_='focus-middle-title')
     for bottomurl in bottomtags:
         bottomurl = baseurl + bottomurl.attrs["href"]
         bottomurls.append(bottomurl)
-    # news-item
 
-    pageurls = []
+    # news-item
     pagetags = soup.find_all('h3', class_='title-news')
     for pageurl in pagetags:
         pageurl = baseurl + pageurl.a.attrs["href"]
         print("fetching " + pageurl)
         pageurls.append(pageurl)
+    if pagetags == []:
+        return pageurls
+    else:
+        i += 1
+        url = url[:45] + str(i) + ".htm"
+        return fetchpage(url, i, bottomurls, pageurls)
 
-    url = url[:45] + str(i) + ".htm"
-    print(url)
-    fetchpage(url, i+1)
 
-
-fetchpage(url1, 2)
+def returndata():
+    urls = fetchpage(url1, 1, bottomurls, pageurls)
+    return urls
--- web.py
+++ web.py
@@ -2,36 +2,48 @@ import requests
 from bs4 import BeautifulSoup
 from PIL import Image
 import os
-import home
-url = "https://tuoitre.vn/tet-do-an-ngap-mat-giam-can-lam-sao-20200102101843755.htm"
-
-# find the title
+import lmao
 
 
-req = requests.get(url)
-soup = BeautifulSoup(req.content, 'html.parser')
-# find image tag
-vcs = soup.find_all('div', class_='VCSortableInPreviewMode')
-# find date and time tag
-datetime = soup.find('div', class_='date-time').text
-# reformat datetime
-datetime = ' '.join(datetime.split()[0:2])
-datetime = datetime.replace("/", "-")
-datetime = datetime.replace(":", "")
-# make a folder with datetime
-if os.path.isdir(f'./{datetime}') == False:
-    os.mkdir(f'./{datetime}')
-images = []
-n = len(vcs) - 1
-vcs = vcs[:n]
-for i, vc in enumerate(vcs):
-    # find caption and save to .txt
-    cap = vc.find('p').text
-    print(cap)
-    with open(f'./{datetime}/caption{i}.txt', 'a', encoding='utf-8') as f:
-        f.write(cap)
-    # find and save image
-    image_url = vc.div.img['src']
-    img = Image.open(requests.get(image_url, stream=True).raw)
-    print(img)
-    img.save(f'./{datetime}/image{i}.jpg')
+def download(url):
+    # find the title
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    # find image tag
+    vcs = soup.find_all('div', class_='VCSortableInPreviewMode')
+    # find date and time tag
+    datetime = soup.find('div', class_='date-time').text
+    # reformat datetime
+    datetime = ' '.join(datetime.split()[0:2])
+    datetime = datetime.replace("/", "-")
+    datetime = datetime.replace(":", "")
+    # make a folder with datetime
+    if os.path.isdir(f'./{datetime}') == False:
+        os.mkdir(f'./{datetime}')
+    images = []
+    n = len(vcs) - 1
+    vcs = vcs[:n]
+    for i, vc in enumerate(vcs):
+        # find caption and save to .txt
+        cap = vc.find('p').text
+        with open(f'./{datetime}/caption{i}.html', 'a', encoding='utf-8') as f:
+            f.write(cap)
+        # find and save image
+        image_url = vc.div.img['src']
+        img = Image.open(requests.get(image_url, stream=True).raw)
+        img.load()
+        img.save(f'./{datetime}/image{i}.jpg')
+
+
+def exec():
+    urls = lmao.returndata()
+    print("________________________________________________________")
+    print("Downloading")
+    print("________________________________________________________")
+    for i in urls:
+        print("downloading " + i)
+        download(i)
+    pass
+
+
+exec()
